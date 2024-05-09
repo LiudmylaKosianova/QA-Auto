656e60f27d4d0137dfa98a424eb04a72af66e404
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-09 10:30:28 +0200

    project

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -8,8 +8,8 @@ class User:
         self.second_name = None
 
     def create(self):
-        self.name = "Sergii"
-        self.second_name = "Butenko"
+        self.name = "Liudmyla"
+        self.second_name = "Kosianova"
 
     def remove(self):
         self.name = ""
