e5cb564829ab3462d3a9bff64b6659a26cee91e9
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-16 20:22:51 +0200

    tests http

--- project/tests/api/test_http.py
+++ project/tests/api/test_http.py
@@ -5,3 +5,10 @@ import requests
 def test_first_request():
     r = requests.get("https://api.github.com/zen")
     print(r.text)
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get("https://api.github.com/users/defunkt")
+    print(f"Response body: {r.json()}")
+    print(f"Response status code: {r.status_code}")
+    print(f"Response headers: {r.headers}")
