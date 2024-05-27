0e21942fd7092df6706bde2e1f25115ce33f9594
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-27 10:04:43 +0200

    new github methods

--- project/modules/api/clients/github.py
+++ project/modules/api/clients/github.py
@@ -2,12 +2,18 @@ import requests
 
 class Github:
 
-    def get_user_defunkt(self):
-        r = requests.get('http://api.github.com/users/defunkt')
-        body = r.json()
-        return body
+    # def get_user_defunkt(self):
+    #     r = requests.get('http://api.github.com/users/defunkt')
+    #     body = r.json()
+    #     return body
     
-    def get_non_exist_user(self):
-        r = requests.get('http://api.github.com/users/snowwhiteship')
+    # def get_non_exist_user(self):
+    #     r = requests.get('http://api.github.com/users/snowwhiteship')
+    #     body = r.json()
+    #     return body
+
+    def def_user(self, username):
+        r = requests.get(f"http://api.github.com/users/{username}")
         body = r.json()
+
         return body
