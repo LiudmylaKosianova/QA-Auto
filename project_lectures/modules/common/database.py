fea82384c240e1fead0abec0bf202df4b1f1232a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-05-27 19:29:41 +0200

    database

--- project_lectures/modules/common/database.py
+++ project_lectures/modules/common/database.py
@@ -5,11 +5,21 @@ class Database():
     def __init__(self):
         self.connection = sqlite3.connect(r"/home/carmin/Public/QA-Auto/project_lectures"+r"become_qa_auto.db")
         self.cursor = self.connection.cursor()
+
     def test_connection(self):
         sqlite_select_query = "SELECT sqlite_version();"
         self.cursor.execute(sqlite_select_query)
         record = self.cursor.fetchall()
         print(f"Success. SQLite Database version: {record}")
+
+    def get_all_users(self):
+        query = "SELECT name, address, city FROM customers;"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+     
+
+    
 
     
 #print(__name__)
---
+++ project_lectures/modules/common/investigate.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+connection = sqlite3.connect(r"/home/carmin/Public/QA-Auto/project_lectures"+r"become_qa_auto.db")
+cursor = connection.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = cursor.fetchall()
+
+print(tables)
