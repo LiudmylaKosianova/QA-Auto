1499ba412b65bba3fc8b3e6ab13adc469fe2265a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-06-01 08:50:04 +0200

    deleted some folders

--- project_lectures/modules/common/investigate.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect(r"/home/carmin/Public/QA-Auto/project_lectures"+r"/become_qa_auto.db")
-cursor = connection.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
-
-print(tables)
