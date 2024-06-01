2d0514f7773644894ca7e06aac70aa18bd4f8880
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-06-01 07:57:56 +0200

    project individual task 6

--- project/modules/ui/page_objects/sign_in_page.py
+++ project/modules/ui/page_objects/sign_in_page.py
@@ -6,15 +6,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 class SignInPage(BasePage):    
-    URL = "https://github.com/login"
+    #URL = "https://github.com/login"
 
     def __init__(self) -> None:
         super().__init__()
 
-    def go_to(self, ):
-        self.driver.get(SignInPage.URL)
+    def go_to(self,URL):
+        self.driver.get(URL)
 
-    def try_sign_in(self, username, password):
+    def try_sign_in_GitHub(self, username, password):
         username_element = self.driver.find_element(By.ID, "login_field")
         username_element.send_keys(username)
 
@@ -23,6 +23,10 @@ class SignInPage(BasePage):
 
         sign_in_element = self.driver.find_element(By.NAME, "commit")
         sign_in_element.click()
+    
+    def find_sign_in_Amazon(self):
+        signin_element = self.driver.find_element(By.ID, "nav-link-accountList-nav-line-1")
+        signin_element.click()
 
     def check_title(self, title_expected):
         return self.driver.title == title_expected
--- project/tests/ui/test_ui_page_object.py
+++ project/tests/ui/test_ui_page_object.py
@@ -2,9 +2,18 @@ import pytest
 from modules.ui.page_objects.sign_in_page import SignInPage
 
 @pytest.mark.ui 
-def test_sign_in_wrong_name():
+def test_sign_in_GitHub_wrong_name():
     signin = SignInPage()
-    signin.go_to()
-    signin.try_sign_in("wrongmail@wrong.com", "wrongpassword")
+    signin.go_to("https://github.com/login")
+    signin.try_sign_in_GitHub("wrongmail@wrong.com", "wrongpassword")
     signin.check_title("Sign in to GitHub · GitHub")
+    signin.close()
+
+@pytest.mark.ui 
+def test_sign_in_Amazon():
+    signin = SignInPage()
+    signin.go_to("https://www.amazon.com/ref=nav_logo")
+    signin.check_title("Amazon.com.Spend less. Smile more")
+    signin.find_sign_in_Amazon()
+    signin.check_title("Amazon Sign-In")
     signin.close()
